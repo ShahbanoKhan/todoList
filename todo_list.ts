851ace8415191bb878517693ca65eb4684d7995b
sorceryspecter <jehanzebshahbano@gmail.com>
2024-04-14 19:36:33 +0500

    push todo list to github

---
+++ todo_list.ts
@@ -0,0 +1,57 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import CheckboxPrompt from "inquirer/lib/prompts/checkbox.js";
+
+let todo: any[] = [];
+
+let condition = true;
+
+while (condition) {
+    let addTask  = await inquirer.prompt(
+    [
+        {
+            name: 'todo',
+            type:'input',
+            message: 'What do you want to add in your to-do list'
+        },
+
+        {
+            name: 'addMore',
+            type: 'confirm',
+            message: 'Do you want to add more?',
+            default: 'true'
+        },
+    ]
+    );
+    
+    todo.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todo);
+    
+};
+
+let options = await inquirer.prompt(
+    {
+        name: 'option',
+        type: 'list',
+        choices: ['New Task','Task Completed']
+    }
+);
+
+if (options.option === 'New Task') {
+    console.log(todo);
+}
+
+else  {
+    let completedOpt = await inquirer.prompt(
+        [
+            {
+                name: 'done',
+                type: 'checkbox',
+                choices: todo.map(task => ({name: task}))
+            }
+        ]
+    );    
+};
+
